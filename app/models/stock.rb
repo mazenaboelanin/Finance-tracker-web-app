151b905b1195928efa2097b3c6c43642ab9e45e5
mazenaboelanin <mazenaboelanin@gmail.com>
2022-03-16 11:21:47 +0200

    handle invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,6 +8,11 @@ class Stock < ApplicationRecord
         
         # return client.price
         # client.price(ticker_symbol)
-        new(ticker: ticker_symbol , name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        begin
+            new(ticker: ticker_symbol , name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
+
     end
 end
